3d0bd1def8877700fffff05c922eacbac96e6dc1
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2025-02-01 23:01:38 +0100

    vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import Head from 'next/head';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
       </Head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
